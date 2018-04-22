8030a4e3915bd8630d4bc93159ecdd7376241088
wz-dazhi <742820195@qq.com>
2018-04-22 13:36:26 +0800

    栈

---
+++ src/main/java/com/wz/stack/MyStack.java
@@ -0,0 +1,64 @@
+package com.wz.stack;
+
+/**
+ * @projectName: data-structure
+ * @package: com.wz.stack
+ * @className: MyStack
+ * @description: 自定义栈,底层是一个数组 特点:先进后出,后进先出
+ * @author: wangzhi
+ * @createDate: 2018/4/22 13:20
+ **/
+public class MyStack {
+
+    private long[] arr;
+
+    /**
+     * 栈顶
+     */
+    private int top;
+
+    public MyStack() {
+        arr = new long[10];
+        top = -1;
+    }
+
+    public MyStack(int maxSize) {
+        arr = new long[maxSize];
+        top = -1;
+    }
+
+    /**
+     * 添加数据
+     */
+    public void push(int value) {
+        arr[++top] = value;
+    }
+
+    /**
+     * 移除数据,弹出数据
+     */
+    public long pop() {
+        return arr[top--];
+    }
+
+    /**
+     * 查看数据
+     */
+    public long speak() {
+        return arr[top];
+    }
+
+    /**
+     * 是否为空
+     */
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    /**
+     * 是否满了
+     */
+    public boolean isFull() {
+        return top == arr.length - 1;
+    }
+}
---
+++ src/test/java/com/wz/stack/MyStackTest.java
@@ -0,0 +1,61 @@
+package com.wz.stack;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @projectName: data-structure
+ * @package: com.wz.stack
+ * @className: MyStackTest
+ * @description:
+ * @author: wangzhi
+ * @createDate: 2018/4/22 13:25
+ **/
+public class MyStackTest {
+
+    private MyStack stack;
+
+    @Before
+    public void setUp() throws Exception {
+        stack = new MyStack(4);
+        stack.push(25);
+        stack.push(12);
+        stack.push(5);
+        stack.push(23);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void pop() {
+        while (!stack.isEmpty())
+            System.out.print(stack.pop() + ",");
+        System.out.println();
+        System.out.println(stack.isFull());
+        System.out.println(stack.isEmpty());
+    }
+
+    @Test
+    public void speak() {
+        System.out.println(stack.speak());
+    }
+
+    @Test
+    public void isEmpty() {
+        System.out.println(stack.isEmpty());
+    }
+
+    @Test
+    public void isFull() {
+        System.out.println(stack.isFull());
+    }
+}
